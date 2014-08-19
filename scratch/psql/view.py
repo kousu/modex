ea45c5e4d508c7b61fb35dda01c857a7e7c7a4f2
kousu <nick@kousu.ca>
2014-08-18 23:47:23 -0400

    view.py is now executable, in demo; it reads the films table changes feed and spits out the relevant lines
    Getting this to work required beating up the SQL a bit too.

--- scratch/psql/view.py
+++ scratch/psql/view.py
@@ -1,53 +1,82 @@
 # view.py
 """
-pure-python protoype of the sort of
+pure-python protoype of the sort of features we need in a (near-)real-time View.
+A standard
 """
 
+import sys
+import json #for IPC; this will get replaced with something more tuned for high-volume later
+#import sqlalchemy
 
+import select
 
-class ChangeEvent(object):
+from itertools import count
+
+# TODO: support replicators
+
+class ChangeEvent(object): #abstract base class
     pass
     
 class Insert(ChangeEvent):
     def __init__(self, row):
         self.row = row
+    def __str__(self):
+        return json.dumps({"+": self.row})
         
 class Delete(ChangeEvent):
     def __init__(self, row):
         self.row = row
+    def __str__(self):
+        return json.dumps({"-": self.row})
         
 class Update(ChangeEvent):
     def __init__(self, old, new):
         self.old = old
         self.new = new
-
+    def __str__(self):
+        return json.dumps({"-": self.old, "+": self.new})
 
 class ITableChanges(object):
     def next(self):
         raise NotImplementedError("This is an interface definition only. Subclass and override.")
 
 
-import json
 class PipeChanges(ITableChanges):
     "parses change events as serialized by watch.psql"
     """
      the serialization format is:
-      1 byte giving the type of event (+, - for delete or > for change)
+      a json object that looks like:
+        {"+": row} for inserts, or
+        {"-": row} for deletes, or
+        {"-": row, "+", row} for updates
+      where a row is a jsonification of a SQL row, ie.
+     This is not the most efficient protocol we could use, but it's dead simple to program against.
     """
     def __init__(self, stream):
         "stream should be an open file object in +r mode"
         self._source = stream
-    def next(self):
+    
+    def __next__(self):
         
-        evt = self._source.readline() #blocks here; we could use select() but since we only wait on one 
-        type, row = evt[0], evt[1:]
-        row = json.loads(row)
-        if type == "+":
-            return Insert(row)
-        elif type == "-":
-            return Delete(row)
-        elif type == ">":
-            row = json.loads
+        evt = ""
+        while not evt: #hack around empty lines being a problem?!
+            a,b,c = select.select([self._source], [], []) #blocks until self._source is ready
+            print("select result:", a, b, c)
+            evt = self._source.readline() #blocks here; we could use select() but since we only wait on one 
+        # XXX bug: readline() doesn't block on EOF (which python passes us as "")
+        # patch: use `tail -f` instead of `cat`
+        # XXX flakiness: running multiple readers can make the other reader steal some of the bytes and crash the JSON deserialization
+        # XXX flakiness(?): postgres apparently doesn't call *any* triggers until the session (ie the database connection) that created them is closed(??)
+            evt = evt.strip()
+            print("_next() read '%s'" % (evt,))
+        
+        evt = json.loads(evt)   #TODO: handle exceptions here
+        if "+" in evt and "-" in evt:
+            return Update(evt["-"], evt["+"])
+        elif "+" in evt:
+            return Insert(evt["+"])
+        elif "-" in evt:
+            return Delete(evt["-"])
         else:
             warn("Malformed change event: `%s`" % evt)
 
@@ -79,13 +108,14 @@ class View(object):
       * Ideally, this feature would be fast, in C, inside of Postgres, triggering on updates to parents of MATERIALIZED VIEWS, and would properly handle the full range of SQL (JOINs, SET a = a + 1, etc).
     """
     
-    def __init__(self, parent, columns=None, where=lambda *args: True):
-        "parent is a table to watch"
+    def __init__(self, changes, columns=None, where=lambda *args: True):
+        #"parent is a table to watch"
+        "changes is a stream of changes from a single table that you're watching" #XXX this def'n is awkward; I don't know what is ideal yet.
         "columns is a list of; defaults to all columns"
         "if specified, where should be a predicate taking a row and telling whether it is in the view or not"
-        self._parent = parent
-        if columns is None:
-            columns = parent.get_all_columns_XXX_THIS_CALL_DOESNT_EXIST()
+        self._source = changes
+        #if columns is None:
+        #    columns = parent.get_all_columns_XXX_THIS_CALL_DOESNT_EXIST_and_maybe_it_doesnt_need_to()
         self._columns = columns
         self._where = where
     
@@ -94,19 +124,35 @@ class View(object):
         self._queue.push(evt) #TODO: locking, etc
     
     def _publish(self, evt):
+        if evt is None: return #silently make empty events into no-ops 
         print("publishing", evt)
         pass #???
+    
+    
+    def _cull(self, row):
+        "reduce a row (given as a dictionary) to only the columns (ie dict keys) that are in self.columns)"
+        if self._columns: #if *not* self._columns, pass through all columns
+            row = {k: row[k] for k in self._columns} #XXX this might be a speed bottleneck
+        return row
         
     def _process(self):
-        evt = self._queue.pop(0) #TODO: locking/blocking (python's got a native library for this, right?)
+        evt = next(self._source)
+        #evt = self._queue.pop(0) #TODO: locking/blocking (python's got a native library for this, right?)
         # the code below must happen at some point... maybe not in a subroutine like this. then again, maybe so.
         # evt is a change event; for our purposes, this means it has a .type field which is either ">" (update), "+" (insert) or "-" (delete) followed by
         
-        assert (isinstance(evt, ChangeEvent)) and (not type(evt) is ChangeEvent)
+        if not (isinstance(evt, ChangeEvent) and (not type(evt) is ChangeEvent)):
+            raise TypeError
         
+        # map source events to events on the table that the view is pretending to be
+        # that is: censor events on rows which the view is not interested in,
+        #          cull rows down to , and
+        #          potentially convert updates to inserts or deletes
         if isinstance(evt, Insert) or isinstance(evt, Delete): #TODO: polymorphism?
             if self._where(evt.row): #pass inserts and deletes through unscathed, if they pass the filter
-                self._publish(evt)
+                evt = evt
+            else:
+                evt = None #do not publish; this row is not in the view
         elif isinstance(evt, Update):
             # updates are subtler
             # every update could be viewed as a deletion and insertion, but that's inefficient and has atomicity issues.
@@ -117,10 +163,63 @@ class View(object):
             _oIn = self._where(evt.old) #precache, to avoid hitting the predicate more than necessary
             _nIn = self._where(evt.new)
             if _oIn and _nIn: #both old and new states of the view see this row; pass it through as an update unscathed
-                self._publish(evt)
-            elif _oIn:
-                self._publish(Delete(evt.old))
+                evt = evt
             elif _nIn:
-                self._publish(Insert(evt.new))
+                evt = Insert(evt.new)
+            elif _oIn:
+                evt = Delete(evt.old)
             else:
-                pass #do not publish; this row is not in the view
+                evt = None #do not publish; this row is not in the view
+        
+        # filter down the rows 
+        if isinstance(evt, Insert) or isinstance(evt, Delete):  #TODO: polymorphism? 'isinstance' is usually a sign that you should be using polymorphism
+            evt = type(evt)(self._cull(evt.row))
+        elif isinstance(evt, Update):
+            evt = type(evt)(self._cull(evt.old), self._cull(evt.new))
+        
+        # XXX ^ the above flow can probably be simplified by rethinking the ChangeEvent hierarchy
+        
+        self._publish(evt)
+
+
+
+if __name__ == '__main__':
+    #view() (pretend view.py is a function which takes some args and then spits a stream back; that's actually jsut waht it does niggah) 
+    #def view([columns], table, where=None)
+    #  - zeroth step is to parse the initial lines from the client, which contain the query: (columns, table, where)
+    #  - first step is it tells the query to the DB ("select [columns] from table where ...")
+    #  - second it arranges to receive the feed (note: feed == stream == queue; it is ordered and waits for us to pull from it)) of changes for the given table ((this might involve setting up triggers? it also might involve speaking to 'teed')); I *think* the order is important here: we want to receive changes beginning after the query happened. Unless we specifically create (perhaps in the same query?).
+    #  - third it spools out the result of the query to the client, as a stream of + changes; because of [MVCC](https://wiki.postgresql.org/wiki/MVCC), this spools out
+    #  - finally, it transparently switches over to spinning through the feed of changes, doing the filtering as it goes
+    
+    # STEP ZERO: parse request
+    # hardcoded columns
+    columns = ["name", "rating"] #nb. None ~= all
+    
+    # hardcoded table
+    table = "films"
+    
+    # hardcoded where predicate
+    def w(row):
+        print("where() clause received row", row)
+        return row["rating"] >= 3
+    
+    # ** the above three should be replaced by some code which parses the initial request    
+    
+    # STEP ONE
+    # ** we skip doing this for now; it is as if the table always starts empty, which is fine for testing
+    
+    # STEP TWO:
+    # ** we hardcode directly reading from the changes feed
+    with open("data/_changes_%s" % (table,)) as feed:
+        # STEP THREE: spool the initial DB state (ie its state at query-time) out
+        # ** skipped, since step two is skipped
+        
+        # STEP FOUR: spool changes out
+        #
+        v = View(PipeChanges(feed), where=w)
+        for i in count():
+            print("-"*80)
+            print("step ", i)
+            v._process()
+            print("-"*80)
