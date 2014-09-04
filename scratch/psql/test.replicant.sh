cd3a67927a183836a7be55c3ccfe0cd251f052bf
kousu <nick@kousu.ca>
2014-09-04 02:37:29 -0400

    Cleaning up the psql directory, in prep for merging into main

--- scratch/psql/test.replicant.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# demonstrate how to run the replicant
-
-echo '{"table": "films"}' | ./replicant.py 2>/dev/null
-
-# in future, this will probably be:
-#./replicant.py "films" 2>/dev/null
