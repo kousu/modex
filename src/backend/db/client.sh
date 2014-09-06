a6c629e7d7b434a6d4c8ae18e3530e4fd3f0452e
kousu <nick@kousu.ca>
2014-09-06 17:19:25 -0400

    *undo* my forced-cd in thre postgres client.sh
    This made test.psql annoying to use, because client.sh would basically lie
    about where it was running from, and test.psql relies on \COPY being able
    to use relative paths.

--- src/backend/db/client.sh
+++ src/backend/db/client.sh
@@ -4,14 +4,14 @@
 #  server.sh must be running for this to work
 
 pushd $(dirname $0) >/dev/null; HERE=`pwd`; popd >/dev/null
-cd $HERE
+#cd $HERE
 
 DB=$1
 if [ -z $DB ]; then
   DB="postgres"  # initdb makes a default database called "postgres" so we just ride on that as the default
 fi
 
-PGDATA=`pwd`/data
+PGDATA=$HERE/data
 
 psql -d $DB -h $PGDATA #<-- this construction makes -h start with a slash which makes postgres interpret it as a path instead of an address which means psql connects over unix domain instead of tcp
 
